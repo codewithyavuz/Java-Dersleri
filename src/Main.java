01df648c1d148a6d46dcea9759d0aaddf42f17a2
Yavuz Ozmen <yvz.o@Yavuz-MacBook-Pro.local>
2025-03-09 20:46:45 +0100

    customer url change

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Merhaba benim amacim degisim");
-        System.out.println("Ikinci Degisim");
+        System.out.println("Sadece ben  varim");
      }
 }
