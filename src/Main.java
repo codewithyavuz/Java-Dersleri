1258492fe6f9cfe992940e1d877780870b7a140d
Yavuz Ozmen <yvz.o@Yavuz-MacBook-Pro.local>
2025-03-10 04:49:34 +0100

    Yeni Degisklik

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Sadece ben  varim");
         System.out.println("Ikinci degisim icindeyim");
+        System.out.println("Yeniden duzenleme");
      }
 }
